5848e6c097909e4fea0fc78777bd17a9ccb576b4
David OC <davidherzlos@gmail.com>
2021-07-02 22:46:57 -0500

    Working...

--- tests/test_rope.py
+++ tests/test_rope.py
@@ -44,6 +44,10 @@ class Concatenation(Rope):
     def __str__(self):
         return str(self.left) + str(self.right)
 
+# Testing Framework
+def equals():
+    pass
+
 assert str(to_rope("abc")) == "abc"
 assert str(to_rope("abcde").substring(1, 3)) == "bcd"
 assert str(to_rope("abcde").substring(1, 3).substring(1, 1)) == "c"
