14746f7ccbdeb1f1137d4e203a043c1ce7beae97
David OC <davidherzlos@gmail.com>
2021-07-03 09:28:49 -0500

    Working...

--- tests/test_rope.py
+++ tests/test_rope.py
@@ -19,7 +19,7 @@ class Rope(object):
     
     def delete(self, start, length):
         left = self.substring(0, start)
-        right = self.substring(start  + length, self.length() - start - length)
+        right = self.substring(start  + length, self.length() - start - length) # Not very readable
         return left.concatenate(right)
     
     def insert(self, rope, start):
