f538f183e2ac063f79c0eb8de1b288a78276ba97
David OC <davidherzlos@gmail.com>
2021-07-02 22:52:04 -0500

    Working...

--- tests/test_rope.py
+++ tests/test_rope.py
@@ -55,6 +55,6 @@ def equals(rope, expected):
     
 
 equals(to_rope("abc"), "abc")
-assert str(to_rope("abcde").substring(1, 3)) == "bcd"
-assert str(to_rope("abcde").substring(1, 3).substring(1, 1)) == "c"
-assert str(to_rope("abc").concatenate(to_rope("de"))) == "abcde"
+equals(to_rope("abcde").substring(1, 3), "bcd")
+equals(to_rope("abcde").substring(1, 3).substring(1, 1), "c")
+equals(to_rope("abc").concatenate(to_rope("de")), "abcde")
