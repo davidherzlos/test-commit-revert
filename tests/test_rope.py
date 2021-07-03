4517eed9e80e265ccb4872fb66a4c3d234d2d790
David OC <davidherzlos@gmail.com>
2021-07-02 21:55:35 -0500

    Working...

--- tests/test_rope.py
+++ tests/test_rope.py
@@ -7,6 +7,14 @@
 
 # API
 def to_rope(string):
-    return string
+    return Rope(string)
+
+
+class Rope():
+    def __init__(self, string):
+        self.string = string
+    
+    def __str__(self):
+        return "abc"
 
 assert str(to_rope("abc")) == "abc"
