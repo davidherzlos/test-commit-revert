5a28145de5220ac9b9d417fe96e8be55dd23cdc9
David OC <davidherzlos@gmail.com>
2021-07-04 10:49:44 -0500

    Working...

--- tests/test_rope.py
+++ tests/test_rope.py
@@ -18,9 +18,10 @@ class Rope(object):
         left = self[0:start]
         right = self[start : len(self)]
         return left + rope + right
+    
     def _get_single_item(self, index):
         raise Exception('Should have been overriden')
-        
+
     def __add__(self, addend):
         return Concatenation(self, addend)
     
